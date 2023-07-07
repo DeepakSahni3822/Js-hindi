0d519cd7ac346f9097c4bba04a78628d41adc1cf
DeepakSahni3822 <121127098+DeepakSahni3822@users.noreply.github.com>
2023-07-08 01:13:23 +0530

    Update 01_vaiables.js

--- basics/01_vaiables.js
+++ basics/01_vaiables.js
@@ -1,1 +1,5 @@
-const acoount
+const acoountId = "12345"
+let accountPassword = "12345"
+accountCity = "Jaipur"
+var accountEmail = "deepak@google.com"
+console.table([accountEmail, accountPassword, acoountId, accountCity]);
