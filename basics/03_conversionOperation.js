50f86b7f33eb409ab7105fb0a35670ea7a8f75cb
DeepakSahni3822 <121127098+DeepakSahni3822@users.noreply.github.com>
2023-07-09 02:36:42 +0530

    Update 03_conversionOperation.js
    conversion of data types

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,6 +1,19 @@
-let score = 33
+let score = "deepak"
 
-console.log(typeof score);
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+console.log(typeof booleanIsloggedIn);
+
+
